d71b7393e1e1b3bdb328d2a18f47078bfc6841a9
jakekasan <jake.kasan@gmail.com>
2018-06-22 23:58:57 +0200

    added sorting function

--- web2/sketch.js
+++ web2/sketch.js
@@ -13,9 +13,13 @@ function consolidateData(data,newData){
   for (point of newData){
     let dataInData = data.map(x => { dataPointEquals(x,point) }).includes(true);
     if (dataInData) {
-      
+
     }
   }
+}
+
+function isDataNew(currentData,newData){
+  currentData.map(cur => { cur.map() })
 }
 
 function dataPointEquals(pointA,pointB){
@@ -25,4 +29,21 @@ function dataPointEquals(pointA,pointB){
     }
   }
   return false;
+}
+
+function sortData(data){
+  if (data.length < 1) { return [] }
+  let finalData = [];
+  finalData.push(data.pop())
+  while (data.length > 0) {
+    let chosen = data.pop();
+
+  }
+
+}
+
+function sortDataInsert(data,item,sortingFunction){
+  for (let i = 0; i < data.length; i++){
+    
+  }
 }
